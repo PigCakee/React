573229f1719f6230d3d45199df611300bd54e879
PigCakee <cw145@mail.ru>
2020-02-20 21:16:19 +0300

    Revert "Revert "Shop+Tournament+Duels activities and layouts for them""
    This reverts commit e7d43723b31cacbbed3877142cd181f73da8d300.

--- app/src/main/java/com/example/reaction/TournamentActivity.kt
+++ app/src/main/java/com/example/reaction/TournamentActivity.kt
@@ -1,10 +1,40 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.reaction
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class TournamentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        val vibrator = this.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        val canVibrate: Boolean = vibrator.hasVibrator()
+        val milliseconds = 15L
+
+        fun vibrate(){
+            if (canVibrate) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    // API 26
+                    vibrator.vibrate(
+                        VibrationEffect.createOneShot(
+                            milliseconds,
+                            VibrationEffect.DEFAULT_AMPLITUDE
+                        )
+                    )
+                } else {
+                    // This method was deprecated in API level 26
+                    vibrator.vibrate(milliseconds)
+                }
+            }
+        }
     }
 }
