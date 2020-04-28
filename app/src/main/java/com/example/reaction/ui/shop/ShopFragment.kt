2c7aebfcc3b5eb6357f567d7bf15b8b8ba53bac2
PigCakee <cw145@mail.ru>
2020-04-28 19:25:12 +0300

    refactor: shop fragment and view model

--- app/src/main/java/com/example/reaction/ui/shop/ShopFragment.kt
+++ app/src/main/java/com/example/reaction/ui/shop/ShopFragment.kt
@@ -2,21 +2,22 @@
 
 package com.example.reaction.ui.shop
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
-import androidx.viewpager.widget.ViewPager
 import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.example.reaction.R
 import com.example.reaction.databinding.ShopFragmentBinding
+import com.example.reaction.util.vibrator.Vibrator
 import com.example.reaction.util.view.PAGES_NUMBER
+import com.example.reaction.util.viewmodel.viewModel
 
 
 class ShopFragment : Fragment() {
@@ -25,42 +26,32 @@ class ShopFragment : Fragment() {
             = ShopFragment()
     }
 
-    private lateinit var viewPager: ViewPager
-    private lateinit var dotsLayout: LinearLayout
     private lateinit var sliderAdapter: ShopSliderAdapter
     private lateinit var sliderDots: Array<TextView?>
-    private lateinit var viewModel: ShopViewModel
 
+    private val model by viewModel<ShopViewModel>()
+    private lateinit var binding: ShopFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding: ShopFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.shop_fragment, container, false)
-        val view = binding.root
-
-        binding.viewModel = ViewModelProviders.of(this).get(ShopViewModel::class.java)
-        viewModel = binding.viewModel as ShopViewModel
-        viewModel.activity = activity
-        return view
+        binding = DataBindingUtil.inflate(inflater, R.layout.shop_fragment, container, false)
+        binding.model = model
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        dotsLayout = activity!!.findViewById(R.id.dotsLayout)
-        viewPager = activity!!.findViewById(R.id.shopViewPager)
-        sliderAdapter = ShopSliderAdapter(
-            context!!,
-            layoutInflater
-        )
 
-        viewPager.adapter = sliderAdapter
+        sliderAdapter = ShopSliderAdapter(context!!, layoutInflater)
+        binding.shopViewPager.adapter = sliderAdapter
 
         addDotsIndicator()
-        viewModel.setupBuySelectButton()
+        model.setupBuySelectButton()
 
-        viewPager.addOnPageChangeListener(object : OnPageChangeListener {
+        binding.shopViewPager.addOnPageChangeListener(object : OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {}
             override fun onPageScrolled(
                 position: Int,
@@ -70,8 +61,8 @@ class ShopFragment : Fragment() {
             }
             override fun onPageSelected(position: Int) {
                 if (sliderDots.isNotEmpty()){
-                    viewModel.weaponSelected = position
-                    viewModel.setupBuySelectButton()
+                    model.weaponSelected = position
+                    model.setupBuySelectButton()
                     for (i in sliderDots.indices) {
                         if (i == position) sliderDots[i]!!.setTextColor(resources.getColor(R.color.colorWhite))
                         else sliderDots[i]!!.setTextColor(resources.getColor(R.color.colorTransparentWhite))
@@ -79,6 +70,23 @@ class ShopFragment : Fragment() {
                 }
             }
         })
+
+        model.vibratorCommand.observe(this){
+            val vibrator: Vibrator = Vibrator.getInstance()
+            vibrator.vibrate(model.milliseconds)
+        }
+
+        model.preferencesCommand.observe(this){
+            model.sharedPreferences = activity!!.getSharedPreferences(model.preferences, Context.MODE_PRIVATE)
+        }
+
+        model.moneyCommand.observe(this){
+            Toast.makeText(context, "You have not enough money to buy!", Toast.LENGTH_SHORT).show()
+        }
+
+        model.soundCommand.observe(this){
+            //Todo play sound
+        }
     }
 
     private fun addDotsIndicator(){
@@ -89,7 +97,7 @@ class ShopFragment : Fragment() {
             sliderDots[i]!!.textSize = 35f
             sliderDots[i]!!.setTextColor(activity!!.resources.getColor(R.color.colorTransparentWhite))
 
-            dotsLayout.addView(sliderDots[i])
+            binding.dotsLayout.addView(sliderDots[i])
         }
 
         sliderDots[0]!!.setTextColor(resources.getColor(R.color.colorWhite))
--- app/src/main/java/com/example/reaction/ui/shop/ShopViewModel.kt
+++ app/src/main/java/com/example/reaction/ui/shop/ShopViewModel.kt
@@ -1,30 +1,75 @@
 package com.example.reaction.ui.shop
 
-import android.app.Activity
-import android.content.Context
 import android.content.SharedPreferences
-import android.util.Log
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import com.example.reaction.entities.Gun
 import com.example.reaction.entities.Player
 import com.example.reaction.entities.Shop
-import com.example.reaction.util.vibrator.Vibrator
+import com.example.reaction.util.livedata.SingleLiveEvent
+
 
 class ShopViewModel : ViewModel() {
     var weaponSelected = 0
+
     private val player: Player = Player.getInstance()
     private var gun: Gun = Gun.makeGunByNumber(weaponSelected)
-
-    private val milliseconds = 15L
-    private val vibrator: Vibrator = Vibrator.getInstance()
-
     private val shop: Shop = Shop.getInstance()
 
-    private var sharedPreferences: SharedPreferences? = null
-    var activity: Activity? = null
+    val milliseconds = 15L
+    val vibratorCommand = SingleLiveEvent<Void>()
+
+    var sharedPreferences: SharedPreferences? = null
+    var preferences = ""
+    val preferencesCommand = SingleLiveEvent<Void>()
+
+    val moneyCommand = SingleLiveEvent<Void>()
+    val soundCommand = SingleLiveEvent<Void>()
 
     val buySelectButton: ObservableField<String> = ObservableField()
+
+    fun onBuySelectButtonClick(){
+        getPreferences(player.preferences)
+        gun = Gun.makeGunByNumber(weaponSelected)
+
+        if (shop.gunArray[weaponSelected] && buySelectButton.get()!! == "Select"){
+            onSelect()
+        }
+
+        if (!shop.gunArray[weaponSelected] && player.money >= gun.cost){
+            onBuy()
+        }
+
+        if (!shop.gunArray[weaponSelected] && player.money < gun.cost){
+            notifyNoMoney()
+        }
+    }
+
+    private fun onSelect() {
+        vibratorCommand.call()
+        player.gun = gun
+        player.save(sharedPreferences!!)
+        buySelectButton.set("Selected")
+    }
+
+    private fun onBuy() {
+        player.money -= gun.cost
+        onSelect()
+        soundCommand.call()
+        getPreferences(shop.preferences)
+        shop.gunArray[weaponSelected] = true
+        shop.save(sharedPreferences!!)
+    }
+
+    private fun notifyNoMoney(){
+        vibratorCommand.call()
+        moneyCommand.call()
+    }
+
+    private fun getPreferences(preferences: String){
+        this.preferences = preferences
+        preferencesCommand.call()
+    }
 
     fun setupBuySelectButton(){
         if (shop.gunArray[weaponSelected]){
@@ -34,42 +79,5 @@ class ShopViewModel : ViewModel() {
             else buySelectButton.set("Select")
 
         } else buySelectButton.set("Buy")
-    }
-
-    fun onBuySelectButtonPress(){
-        sharedPreferences = activity!!.getSharedPreferences(player.preferences, Context.MODE_PRIVATE)
-        vibrator.activity = activity
-
-        gun = Gun.makeGunByNumber(weaponSelected)
-        if (shop.gunArray[weaponSelected] && buySelectButton.get()!! == "Select"){
-            vibrator.vibrate(milliseconds)
-            player.gun = gun
-            player.save(sharedPreferences!!)
-            Log.d("Gun", "Selected")
-
-            buySelectButton.set("Selected")
-        }
-
-        if (!shop.gunArray[weaponSelected] && player.money >= gun.cost){
-            vibrator.vibrate(milliseconds)
-            player.money -= gun.cost
-            player.gun = gun
-            player.save(sharedPreferences!!)
-            Log.d("Gun", "Purchased")
-            //TODO play buy sound
-
-            sharedPreferences = activity!!.getSharedPreferences(shop.preferences, Context.MODE_PRIVATE)
-
-            shop.gunArray[weaponSelected] = true
-            shop.save(sharedPreferences!!)
-
-            buySelectButton.set("Selected")
-        }
-
-        if (!shop.gunArray[weaponSelected] && player.money < gun.cost){
-            vibrator.vibrate(milliseconds)
-            Log.d("Gun", "Not enough money")
-            //TODO notify player that he has not enough money to buy
-        }
     }
 }
